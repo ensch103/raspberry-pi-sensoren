cdcf7a71b2ca3c94aa8050d04204d91bbebdfafd
Enrica Schmidt <enrica.schmidt@hhu.de>
2022-04-06 14:47:15 +0200

    rs-Werte in results speichern

--- src/MCP3008.py
+++ src/MCP3008.py
@@ -17,12 +17,9 @@ class MCP3008:
         self.spi.open(self.bus, self.device)
         self.spi.max_speed_hz = 1000000  # 1MHz
 
-    def read(self, channel=0):
-        cmd1 = 4 | 2 | ((channel & 4) >> 2)
-        cmd2 = (channel & 3) << 6
-
-        adc = self.spi.xfer2([cmd1, cmd2, 0])
-        data = ((adc[1] & 15) << 8) + adc[2]
+    def read(self, channel = 0):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((adc[1] & 3) << 8) + adc[2]
         return data
 
     def close(self):
