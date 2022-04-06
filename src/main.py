00fb1e5af5ae10b7fb2bc34462cc031288141984
Enrica Schmidt <enrica.schmidt@hhu.de>
2022-04-06 14:28:38 +0200

    main überarbeitet

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from MCP3008 import MCP3008 as adc
+from MCP3008 import MCP3008
 import helper
 from time import sleep
 from datetime import datetime
@@ -20,7 +20,7 @@ channel_mq7   = 1
 channel_mq131 = 2
 channel_mq135 = 3
 
-#adc = MCP3008()
+adc = MCP3008()
 
 try:
     print("Press CTRL+C to abort.")
@@ -41,39 +41,34 @@ try:
 
     while True:
         now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        #TODO: use adc (einkommentieren)
-        #value_lpg  = adc.read(channel_mq4)
-        #value_co   = adc.read(channel_mq7)
-        #value_ozon = adc.read(channel_mq131)
-        #value_lq   = adc.read(channel_mq135)
-        value_lpg = 100
-        value_co = 140
-        value_ozon = 120
-        value_lq = 130
+        dt_string   = now.strftime("%d/%m/%Y %H:%M:%S")
+        value_mq4   = adc.read(channel_mq4)
+        value_mq7   = adc.read(channel_mq7)
+        value_mq131 = adc.read(channel_mq131)
+        value_mq135 = adc.read(channel_mq135)
 
         #10-Bit AD-Wandler: 2^10 = 1024 Werte: Spannung = (ADC Wert ÷ 1023) * Versorgungsspannung
-        volt_lpg  = helper.calc_voltage(value_lpg, vc)
-        volt_co   = helper.calc_voltage(value_co, vc)
-        volt_ozon = helper.calc_voltage(value_ozon, vc)
-        volt_lq   = helper.calc_voltage(value_lq, vc)
+        volt_mq4   = helper.calc_voltage(value_mq4, vc)
+        volt_mq7   = helper.calc_voltage(value_mq7, vc)
+        volt_mq131 = helper.calc_voltage(value_mq131, vc)
+        volt_mq135 = helper.calc_voltage(value_mq135, vc)
 
-        rs_lpg  = helper.calc_resistance(volt_lpg, rl_mq4, vc)
-        rs_co   = helper.calc_resistance(volt_co, rl_mq7, vc)
-        rs_ozon = helper.calc_resistance(volt_ozon, rl_mq131, vc)
-        rs_lq   = helper.calc_resistance(volt_lq, rl_mq135, vc)
+        rs_mq4   = helper.calc_resistance(volt_mq4, rl_mq4, vc)
+        rs_mq7   = helper.calc_resistance(volt_mq7, rl_mq7, vc)
+        rs_mq131 = helper.calc_resistance(volt_mq131, rl_mq131, vc)
+        rs_mq135 = helper.calc_resistance(volt_mq135, rl_mq135, vc)
 
         print("volt_LPG: %.2fV    volt_CO: %.2fV    volt_Ozon: %.2fV    volt_LQ: %.2fV" % (volt_lpg, volt_co, volt_ozon, volt_lq)) #TODO: remove
-        print("Rs_LPG: %.2f Ohm   Rs_CO: %.2f Ohm   Rs_Ozon: %.2f Ohm   Rs_LQ: %.2f Ohm" % (rs_lpg, rs_co, rs_ozon, rs_lq)) #TODO: remove
+        print("Rs_LPG: %.2f Ohm   Rs_CO: %.2f Ohm   Rs_Ozon: %.2f Ohm   Rs_LQ: %.2f Ohm" % (rs_mq4, rs_mq7, rs_mq131, rs_mq135)) #TODO: remove
 
         #für jedes Gas, das mit jedem der Sensoren gemessen werden kann (pro Sensor mehr als einen ppm-Wert)
         #mq4_ppm_lpg  = helper.MQGetPercentage(rs_lpg / ro_lpg, )
-        mq7_ppm_co   = helper.MQGetPercentage(rs_co / ro_co, mq7_steigung_CO, mq7_y_CO)
+        mq7_ppm_co   = helper.MQGetPercentage(rs_mq7 / ro_co, mq7_steigung_CO, mq7_y_CO)
         #mq131_ppm_ozon = helper.MQGetPercentage(rs_ozon / ro_ozon, 2)
         #mq135_ppm_lq   = helper.MQGetPercentage(rs_lq / ro_lq, 3)
 
         with open("results.txt", "a") as file:
-            file.write("%s %f %f %f %f" % (dt_string, mq4_ppm_lpg, mq7_ppm_co, mq131_ppm_ozon, mq135_ppm_lq))
+            file.write("%s %f" % (dt_string, mq7_ppm_co))
         print("ppm co:", mq7_ppm_co) #TODO: remove
         sleep(2)
 
